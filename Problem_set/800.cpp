bf2a9b1ba558976f2ee8c8f1ae306428cefdea98
Harsh Bhardwaj <benzenediazonium28@gmail.com>
2026-04-30 21:46:31 +0530

    Add new problem

--- Problem_set/800.cpp
+++ Problem_set/800.cpp
@@ -706,3 +706,35 @@ int main (){
     cout << total << endl;
     return 0 ;
 }
+
+//problem no .510
+//time compelxity  - O(n)
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n , m ;
+    cin >> n >> m ;
+    for (int i = 1 ;i <=n ; i++){
+        if (i%2==1){
+            for (int j = 1 ; j <=m ; j++){
+                cout << "#";
+            }
+            cout << endl;
+        }
+        else if (i%4==2){
+            for (int j = 1 ; j<m; j++){
+                cout << ".";
+            }
+            cout << "#"<<endl;
+        }
+        else {
+            cout << "#";
+            for (int j = 1 ; j < m ; j++){
+                cout << ".";
+            }
+            cout << endl;
+        }
+    }
+    return 0 ;
+}
