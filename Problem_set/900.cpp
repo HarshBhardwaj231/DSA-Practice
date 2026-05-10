f08fac8c7e29e31d0c6cb244f85357947fc90bf7
Harsh Bhardwaj <benzenediazonium28@gmail.com>
2026-05-10 12:11:59 +0530

    Adding new problem

--- Problem_set/900.cpp
+++ Problem_set/900.cpp
@@ -31,3 +31,39 @@ int main(){
     }
     return 0 ; 
 }
+
+//problem no.1837B
+//Time complexity -O(n)
+
+#include<bits/stdc++.h>
+using namespace std;
+int main (){
+    int t ; 
+    cin >> t ;
+    while (t--){
+        int n ; 
+        cin >> n ; 
+        string s ;
+        cin >> s;
+        int count1 = 0 ;
+        int count2 = 0 ;
+        int mx1 =0 ;
+        int mx2 =0 ;
+        for(int i = 0 ; i < n ; i++){
+            if (s[i]=='<'){
+                count1++;
+                mx1=max(count1,mx1);
+            }            else{
+                count1=0;
+            }
+            if(s[i]=='>'){
+                count2++;
+                mx2=max(count2,mx2);
+            }
+            else{
+                count2=0;
+            }
+        }
+        cout << max(mx1,mx2)+1 << endl;
+    }
+}
