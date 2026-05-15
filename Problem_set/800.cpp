84e719902ea26c9fb907cffe0aa390c8f4c1ef6c
Harsh Bhardwaj <benzenediazonium28@gmail.com>
2026-05-15 19:43:36 +0530

    Adding new problem

--- Problem_set/800.cpp
+++ Problem_set/800.cpp
@@ -1009,3 +1009,42 @@ int main(){
     }
     return 0 ;
 }
+
+//problem no. 996A
+//time complexity - O(n)
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n ;
+    cin >> n ;
+    int count= 0 , x=n ;
+    for (int i = 0 ; i < x ; i++){
+        if (n/100 > 0){
+            n=n-100;
+            count++;
+        }
+        else if (n/20 > 0){
+            n=n-20;
+            count++;
+        }
+        else if (n/10 > 0){
+            n=n-10;
+            count++;
+        }
+        else if (n/5 > 0){
+            n=n-5;
+            count++;
+        }
+        else if (n/1 > 0){
+            n=n-1;
+            count++;
+        }
+        else if(n==0){
+            break;
+        }
+    }
+    cout << count << endl;
+    return 0 ;
+}
+    
