a02137e31ccf19ae6a08b018b72fd42511b4c402
Harsh Bhardwaj <benzenediazonium28@gmail.com>
2026-05-17 15:12:55 +0530

    Adding new problem

--- Problem_set/800.cpp
+++ Problem_set/800.cpp
@@ -1325,3 +1325,27 @@ int main(){
      }
     return 0 ;
 }
+
+//problem no.490A
+//time complexity - O(n)
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin >> n ;
+    vector<int>one,two,three;
+    for(int i = 0 ; i < n ; i++){
+        int x ; 
+        cin >> x ;
+        if(x==1){one.push_back(i+1);}
+        else if (x==2){two.push_back(i+1);}
+        else if (x==3){three.push_back(i+1);}
+        }
+    int teams = min({one.size(),two.size(),three.size()});
+    cout << teams << endl;
+    for(int i = 0 ; i < teams ;i++){
+        cout << one[i] << " " << two[i] << " " << three[i] << endl;
+    }
+    return 0;    
+}
