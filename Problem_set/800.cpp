7754e08b29e9acdcddc72c26adbcbf84cbf4c1c8
Harsh Bhardwaj <benzenediazonium28@gmail.com>
2026-05-18 19:46:21 +0530

    Adding new problem

--- Problem_set/800.cpp
+++ Problem_set/800.cpp
@@ -1642,3 +1642,21 @@ int main(){
     }
     return 0 ;
 }
+
+//problem no.472A
+//time complexity -O(n)
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin >> n ;
+    if(n%2 == 0 ){
+        cout << 4 << " " << n-4 ;
+    }
+    else {
+        cout << 9 << " " << n- 9 ;
+    }
+    return 0 ;
+}
