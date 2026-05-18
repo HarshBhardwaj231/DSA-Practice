3a3d506059633b708c4329005c650a7c0a1ebe8f
Harsh Bhardwaj <benzenediazonium28@gmail.com>
2026-05-18 11:47:38 +0530

    Adding new problem

--- Problem_set/800.cpp
+++ Problem_set/800.cpp
@@ -1493,3 +1493,21 @@ int main(){
     }
     return 0 ;
 }
+
+
+//problem no. - 1370A
+//time complexity - O(n)
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t ;
+    cin >> t ;
+    while(t--){
+        int n ;
+        cin >> n ;
+        int total = n/2;
+        cout << total << endl;
+    }
+    return 0 ;
+}
