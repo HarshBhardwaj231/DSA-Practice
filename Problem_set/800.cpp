f52586e0fb3572e3e32ce6e75eae56845bf5cb92
Harsh Bhardwaj <benzenediazonium28@gmail.com>
2026-05-18 14:36:06 +0530

    Adding new problem

--- Problem_set/800.cpp
+++ Problem_set/800.cpp
@@ -1577,3 +1577,23 @@ int main(){
     }
     return 0 ;
 }
+
+//problem no. 1999A
+//time complexity - O(n)
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t ;
+    cin >> t ;
+    while (t--){
+        int n;
+        cin >> n ;
+        int a = n/10 ;
+        int b = n%10;
+        int c = a+b;
+        cout << c << endl;
+    }
+    return 0 ;
+}
