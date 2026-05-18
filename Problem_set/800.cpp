c3c99fd156fc27e30e887d6b2fc051844081b9b4
Harsh Bhardwaj <benzenediazonium28@gmail.com>
2026-05-18 12:48:20 +0530

    Adding new problem

--- Problem_set/800.cpp
+++ Problem_set/800.cpp
@@ -1511,3 +1511,31 @@ int main(){
     }
     return 0 ;
 }
+
+//problem no.1900A
+//time complexity - O(n)
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t ;
+    cin >> t ;
+    while(t--){
+        int n ;
+        cin >> n ;
+        string s;
+        cin >> s;
+        int count = 0 ;
+        for(int i = 0 ; i < n ; i++){
+            if(s[i]=='.' && s[i]==s[i-1] && s[i]==s[i+1]){
+                count = 2 ;
+                break;
+            }
+            else if (s[i]=='.'){
+                count++;
+            }
+        }
+        cout << count << endl;
+    }
+    return 0 ;
+}
