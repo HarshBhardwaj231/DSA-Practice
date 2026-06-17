30fc6d87bb7fb6490cfe7f7507ee39e4cc07621f
Harsh Bhardwaj <benzenediazonium28@gmail.com>
2026-06-17 13:54:00 +0530

    Adding new problems

--- Problem_set/800.cpp
+++ Problem_set/800.cpp
@@ -1683,3 +1683,26 @@ int main(){
     }
     return 0 ;
 }
+
+//problem no. 1676A
+//time complexity - O (n)
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t; 
+    cin >> t; 
+    while (t--){
+        string s ;
+        cin >> s ;
+        int x = s[0] + s[1] + s[2] ;
+        int y = s[3] + s[4] + s[5] ;
+        if(x == y){
+            cout << "YES" << endl;
+        }
+        else {
+            cout << "NO" << endl;
+        }
+    }
+    return 0 ;
+}
