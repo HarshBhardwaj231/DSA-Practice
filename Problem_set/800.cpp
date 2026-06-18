57562baaab75d7f6f07c56f94515a37d296a0924
Harsh Bhardwaj <benzenediazonium28@gmail.com>
2026-06-18 11:47:18 +0530

    adding problems

--- Problem_set/800.cpp
+++ Problem_set/800.cpp
@@ -1754,3 +1754,24 @@ int main(){
     }
     return 0 ;
 }
+
+// problem no. 1791A
+// time complexity - O(n)
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin >> t ;
+    while(t--){
+        char s ;
+        cin >> s; 
+        if((s == 'c') || (s == 'o') || (s == 'd') || (s == 'e') || (s == 'f') || (s == 'o') || (s == 'r') || (s == 'c') || (s == 'e') || (s == 's')){
+            cout << "YES" << endl;
+        }
+        else {
+            cout << "NO" << endl;
+        }
+    }
+    return 0 ;
+}
