43183167ef7d49869ba5fd4e068d043a0778a324
Harsh Bhardwaj <benzenediazonium28@gmail.com>
2026-06-18 10:26:39 +0530

    Adding new problem

--- Problem_set/800.cpp
+++ Problem_set/800.cpp
@@ -1732,3 +1732,25 @@ int main(){
     }
     return 0 ;
 }
+
+
+//problem no. 1807A
+//time complexity - O(n)
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin >> t ;
+    while(t--){
+        int a , b , c ;
+        cin >> a >> b >> c ;
+        if(a+b==c){
+            cout << "+" <<endl;
+        }
+        else if(a-b==c){
+            cout << "-" <<endl;
+        }
+    }
+    return 0 ;
+}
