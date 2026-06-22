f89a80b742a34ce5dd9500a998cc9a757162b7a9
Harsh Bhardwaj <benzenediazonium28@gmail.com>
2026-06-22 23:43:31 +0530

    Adding new probolem

--- Problem_set/800.cpp
+++ Problem_set/800.cpp
@@ -1904,3 +1904,33 @@ int main (){
     }
     return 0 ;
 }
+
+
+//problem no.32B 
+// time complexity - O(n)
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+
+    int n = s.size();
+
+    for(int i = 0; i < n; i++) {
+        if(s[i] == '.') {
+            cout << 0 ;
+        }
+        else {
+            if(s[i + 1] == '.') {
+                cout << 1 ;
+            }
+            else{
+                cout << 2 ;
+            }
+            i++;
+        }
+    }
+    return 0;
+}
