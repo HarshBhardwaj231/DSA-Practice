64e4991975c0f618f16569ea44cad2440b0b8b0f
Harsh Bhardwaj <benzenediazonium28@gmail.com>
2026-06-23 23:29:42 +0530

    Adding new

--- Problem_set/800.cpp
+++ Problem_set/800.cpp
@@ -1934,3 +1934,25 @@ int main() {
     }
     return 0;
 }
+
+
+//problem no. 1345A 
+//time complexity - O (n)
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t ;
+    cin >> t ;
+    while(t--){
+        int n , m ;
+        cin >> n >> m ;
+        if (n < 2 || m < 2 (n == 2 && m == 2 )){
+            cout << "YES" << endl ;
+        }
+        else{
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
