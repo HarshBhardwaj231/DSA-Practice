10c6b9f4401210a8379b145a304a053d104fcbb2
Harsh Bhardwaj <benzenediazonium28@gmail.com>
2026-06-26 15:11:17 +0530

    Adding new problem

--- Problem_set/800.cpp
+++ Problem_set/800.cpp
@@ -2151,3 +2151,26 @@ int main(){
     }
     return 0 ;
 }
+
+//problem no. 1389A
+//time complexity - O(n)
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int l , r ;
+        cin >> l >> r ; 
+        int x = l ;
+        int y = 2*l;
+        if(y<= r){
+            cout << x << " " << y << endl;
+        }
+        else {
+            cout << -1 << " "<< -1 << endl;
+        }
+    }
+    return 0 ;
+}
