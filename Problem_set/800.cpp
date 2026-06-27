c4c96a2b050e2e11827ece349adc3f346c9dd3d6
Harsh Bhardwaj <benzenediazonium28@gmail.com>
2026-06-27 22:20:26 +0530

    Adding new problem

--- Problem_set/800.cpp
+++ Problem_set/800.cpp
@@ -2355,3 +2355,32 @@ int main(){
     }
     return 0;
 }
+
+//problem no. 1927A
+// time complexity - O(n)
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t ; 
+    cin >> t ;
+    while(t--){
+        int n;
+        cin >> n ;
+        string s ;
+        cin >> s;
+        int  a, b ;
+        for(int i = 0 ; i < n ; i++){
+            if(s[i]=='B'){
+                a = i;
+                break;}
+        }
+        for(int i = n-1 ; i >= 0 ; i--){
+            if(s[i]=='B'){
+                b = i;
+                break;}
+        }
+       cout << b - a + 1 << endl ;
+    }
+    return 0;
+}
