0e8d45af3b7f01109bc67e53d3b73873bfe1c867
Harsh Bhardwaj <benzenediazonium28@gmail.com>
2026-06-27 13:37:48 +0530

    Adding new problemm

--- Problem_set/800.cpp
+++ Problem_set/800.cpp
@@ -2207,3 +2207,27 @@ int main(){
     }
     return 0 ;
 }
+
+//problem 1992A
+//time complexity - O(n)
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        vector<int>v;
+        for(int i = 0 ; i < 3 ; i++){
+            int x ; 
+            cin >> x ;
+            v.push_back(x);
+        }
+        for(int i = 0 ; i < 5 ; i++){
+            sort(v.begin(),v.end());
+            v[0]++;
+        }
+         cout << v[0]*v[1]*v[2] << endl;
+    }
+    return 0 ;
+}
