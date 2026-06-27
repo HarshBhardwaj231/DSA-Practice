bb3213e6cf3e68150d083c050572f3c230e0c26f
Harsh Bhardwaj <benzenediazonium28@gmail.com>
2026-06-27 23:35:56 +0530

    Adding new problem

--- Problem_set/800.cpp
+++ Problem_set/800.cpp
@@ -2384,3 +2384,31 @@ int main(){
     }
     return 0;
 }
+
+//problem no. 1777A
+//time complexity - O(n)
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t ; 
+    cin >> t ;
+    while(t--){
+        int n ; 
+        cin >> n ; 
+        vector<int>v;
+        for(int i = 0;i < n ; i++){
+            int x ;
+            cin >> x ;
+            v.push_back(x);
+        }
+        int pair =  0 ;
+        for(int i = 0 ; i < n-1 ; i++){
+            if(((v[i]%2==0)&&(v[i+1]%2==0)) || ((v[i]%2 == 1 )&&(v[i+1]%2 == 1))){
+                pair++; 
+            }
+        }
+        cout << pair << endl;
+    }
+    return 0;
+}
