fc6b0e8aa3f888bcb3dc3c1382a8b9e0ee550af1
Harsh Bhardwaj <benzenediazonium28@gmail.com>
2026-07-01 23:32:20 +0530

    Adding a problem

--- Problem_set/800.cpp
+++ Problem_set/800.cpp
@@ -2588,3 +2588,40 @@ int main() {
     }
     return 0;
 }
+
+//problem no.1374B
+//time complexity - O(n)
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t ;
+    cin >> t ;
+    while (t--){
+        int n ;
+        cin >> n ;
+        int count = 0 ; 
+        if(n==1){
+            cout << 0 << endl;
+        }
+        else{
+           while(n!=1){
+               if(n%6==0){
+                   n=n/6;
+                   count++;
+               }
+               else if (n%3==0){
+                   n=n*2;
+                   count++;
+               }
+               else {
+                   count=-1;
+                   break;
+               }
+           }
+            cout << count << endl;
+        }
+    }
+    return 0 ;
+}
+
