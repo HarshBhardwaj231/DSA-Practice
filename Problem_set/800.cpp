be194ac65dedd27c654f73934b696b55097d5a0b
Harsh Bhardwaj <benzenediazonium28@gmail.com>
2026-07-02 23:11:49 +0530

    Adding new one

--- Problem_set/800.cpp
+++ Problem_set/800.cpp
@@ -2650,3 +2650,24 @@ int main(){
     }
     return 0 ;
 }
+
+//problem no.1475B
+//time complexity - O(n)
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t ; 
+    cin >> t ;
+    while(t--){
+        int n ;
+        cin >> n ;
+        if(n/2020 >= n%2020){
+            cout << "YES" << endl;
+        }
+        else {
+            cout << "NO" << endl;
+        }
+    }
+    return 0 ;
+}
