01707776cc30fc75146daaf5c6906631a0693d02
Harsh Bhardwaj <benzenediazonium28@gmail.com>
2026-07-02 08:39:24 +0530

    Adding new problem

--- Problem_set/800.cpp
+++ Problem_set/800.cpp
@@ -2625,3 +2625,28 @@ int main(){
     return 0 ;
 }
 
+//problem no. 1878C 
+// time complexity - O(n)
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t ;
+    cin >> t ;
+    while (t--){
+        long long  n , k , x ;
+        cin >> n >> k >> x ;
+        
+        long long min = k * (k+1)/2 ;
+        long long max = k * n -(k-1)*k/2 ;
+        
+        
+        if(min<=x && max>=x){
+            cout << "YES" << endl;
+        }
+        else {
+            cout << "NO" << endl;
+        }
+    }
+    return 0 ;
+}
